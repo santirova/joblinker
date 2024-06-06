66e271a806fa5c3720c8363240474e40afe8a5a7
santilagos <sntlgs@gmail.com>
2024-06-05 21:06:33 -0300

    change cors config

--- server/src/app.js
+++ server/src/app.js
@@ -5,15 +5,13 @@ const {privRouter,userRouter} = require('./routes/index')
 const cookieParser = require('cookie-parser')
 const { authMiddleware } = require('./middlewares/authMiddleware')
 require('dotenv').config()
-const { CLIENT_URL } = process.env
-console.log(CLIENT_URL)
 const app = express()
 // Middlewares
 app.use(express.json())
 app.use(cors({
-    origin:`${CLIENT_URL}`,
-    credentials:true
-}))
+    origin: '*',
+    credentials: true
+}));
 app.use(morgan('dev'))
 app.use(cookieParser())
 
