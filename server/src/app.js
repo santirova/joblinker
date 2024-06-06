729a50d9e6ee07825e19a3e220c788af67078f1b
santilagos <sntlgs@gmail.com>
2024-06-06 02:26:02 -0300

    other config

--- server/src/app.js
+++ server/src/app.js
@@ -14,11 +14,18 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Configurar CORS para permitir solo el origen específico
-app.use(cors({
-    credentials: true,
-    origin: CLIENT_URL, // Especifica el origen permitido
-    methods: "GET, POST, OPTIONS, PUT, DELETE"
-}));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); /* https://viandaexpress.vercel.app, http://localhost:5173 */
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use(morgan('dev'));
 
